441cfafaca3c3e2ab03dbb7305a36a0f57796492
DawidWebb <tomasz.matras@developerweb.pl>
2021-07-19 09:41:43 +0200

    initial commit: add extensions, create app module

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const config = require("./_config");
+const mongoose = require("mongoose");
+
+//* mongoose conect *//
+mongoose.connect(config.db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+// const usersRoutes = require("./routes/users.js");
+// const rangeRoutes = require("./routes/range");
+// const fleetRoutes = require("./routes/fleet");
+// const subcontractorRoutes = require("./routes/subcontractor");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// app.use("/range", rangeRoutes);
+// app.use("/subcontractor", subcontractorRoutes);
+// app.use("/fleet", fleetRoutes);
+// app.use("/users", usersRoutes);
+
+app.listen(8000, () => console.log("Server has started"));
+
+// module.exports = app;
